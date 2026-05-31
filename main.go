d9dc3b108e51f7129ededcbe20fa84d9541b098b
Matthew Moodley <mpmoodley@gmail.com>
2026-05-31 19:45:03 +0200

    feat(main): added middleware, logging endpoint http method limiting

--- main.go
+++ main.go
@@ -3,14 +3,51 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
+//func handlerHealth(w http.ResponseWriter, r *http.Request) {
+//	if r.URL.Path != "/healthz" {
+//		http.NotFound(w, r)
+//		return
+//	}
+//	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+//	w.WriteHeader(http.StatusOK)
+//	body := "OK"
+//	w.Write([]byte(body))
+//}
+
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
+
 	stripedRoot := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/", stripedRoot)
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		if req.URL.Path != "/healthz" {
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(stripedRoot))
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/api/healthz" {
 			http.NotFound(w, req)
 			return
 		}
@@ -19,6 +56,9 @@ func main() {
 		body := "OK"
 		w.Write([]byte(body))
 	})
+
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	s := http.Server{
 		Addr:    ":8080",
