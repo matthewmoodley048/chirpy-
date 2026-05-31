0caa46d1926785213bbbd533118e1656d590f3a0
Matthew Moodley <mpmoodley@gmail.com>
2026-05-31 14:35:37 +0200

    feat(main): added custom handlers

--- main.go
+++ main.go
@@ -7,8 +7,18 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	stripedRoot := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", stripedRoot)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/healthz" {
+			http.NotFound(w, req)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		body := "OK"
+		w.Write([]byte(body))
+	})
 
 	s := http.Server{
 		Addr:    ":8080",
