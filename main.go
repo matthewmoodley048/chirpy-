b80ac6c648bb6639a6208978ad078a70e5c07b7a
Matthew Moodley <mpmoodley@gmail.com>
2026-06-01 20:23:14 +0200

    feat(main): added admin apis and json decoding and encoding

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -18,9 +20,9 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1>    <p>Chirpy has been visited %d times!</p>  </body></html>", cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
@@ -28,16 +30,70 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-//func handlerHealth(w http.ResponseWriter, r *http.Request) {
-//	if r.URL.Path != "/healthz" {
-//		http.NotFound(w, r)
-//		return
-//	}
-//	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-//	w.WriteHeader(http.StatusOK)
-//	body := "OK"
-//	w.Write([]byte(body))
-//}
+func (cfg *apiConfig) handlerValidate(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "" || r.Method == http.MethodGet {
+		http.Error(w, "invalid method", http.StatusBadRequest)
+	}
+
+	type parameters struct {
+		Body string `json: "body"`
+	}
+
+	type errResp struct {
+		Error string `json: "error"`
+	}
+
+	type validResp struct {
+		Valid bool `json: "valid"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respBody := errResp{
+			Error: fmt.Sprintf("%v", err),
+		}
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respBody := errResp{
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+
+	}
+	respBody := validResp{
+		Valid: true,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(dat)
+}
 
 func main() {
 	apiCfg := &apiConfig{}
@@ -56,9 +112,10 @@ func main() {
 		body := "OK"
 		w.Write([]byte(body))
 	})
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidate)
 
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	s := http.Server{
 		Addr:    ":8080",
